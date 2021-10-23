762e0266bbe702d4cd530478d1e303e825b932e6
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-10-24 00:07:29 +0600

    V-1

---
+++ Code of LC.js
@@ -0,0 +1,31 @@
+
+
+//Love Calculator
+
+//Person name
+
+
+let yourname=prompt('What is your Name?');
+
+let friendname=prompt ('What is your Partner Name?');
+
+
+// caculation
+
+let number=Math.random()*100;
+
+number = Math.floor(number)+1;
+
+
+// Showing Result 
+
+alert( yourname + ' and ' + friendname + " ' Your love percentage is:" 
++ number + '%' );
+
+
+
+
+
+
+
+ 
